b9771b6bbfe7f6b3ac03556e3003d4f698048575
NNahkan <hoksuz2424@gmail.com>
2023-12-10 00:46:25 -0500

    Creating post

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,16 +5,27 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
 
 const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
+	Component,
+	pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+	return (
+		<SessionProvider session={session}>
+			<Head>
+				<title>Twitter Clone</title>
+				<meta name="description" content="This is a tw clone by ALi" />
+			</Head>
+			<div className="container mx-auto flex items-start sm:pr-4">
+				<SideNav/>
+				<div className="min-h-screen flex-grow border-x">
+					<Component {...pageProps} />
+				</div>
+			</div>
+		</SessionProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
